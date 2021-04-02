3efe49e8e9345a9f546979810ac26c7e25c66359
Dinuda <ydinuda@gmail.com>
2021-04-02 19:03:34 +0530

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ASM
 Simple Kernal using C
+
+## Run
+You can use VirtualBox to open the ISO file after configring it.
+ISO file is in the repository.
+
+Feel Free to make pull requests and issues.
---
+++ kernel.c
@@ -0,0 +1,50 @@
+// assigns the start address
+// resets terminal location
+#define VGA_ADDRESS 0xB8000 /*video memory begins here */
+
+/* VGA provides support for 16 colours */
+#define BLACK 0 
+#define GREEN 2
+#define RED 4
+#define YELLOW 14
+#define WHITE_COLOR 15 
+
+unsigned short *terminal_buffer;
+unsigned int vga_index;
+
+/* clears the terminal */
+void clear_screen(void)
+{
+    int index = 0;
+    /* there are 25 lines each of 80 coloumns each element takes 2 bytes*/
+    while (index < 80 * 25 * 2) {
+        terminal_buffer[index] = ' ';
+        index += 2;
+    }
+}
+
+// prints the message in one color
+void print_string(char *str, unsigned char color)
+{
+    int index = 0;
+    while (str[index]) {
+        terminal_buffer[vga_index] = (unsigned short)str[index]|(unsigned short)color << 8;
+        index++;
+        vga_index++;
+    }
+}
+
+// sets internal location market to the next line
+// prints another message
+// returns back to the bootcode
+void main(void) 
+{
+    terminal_buffer = (unsigned short *)VGA_ADDRESS;
+    vga_index = 0;
+
+    clear_screen();
+    print_string("Hello from Linux Journal!", YELLOW);
+    vga_index = 80;    /* next line */
+    print_string("Goodbye from Linux Journal!", RED);
+    return;
+}
