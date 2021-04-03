344432338f518b2521598ad4cfc998dabf1861ab
Dinuda <ydinuda@gmail.com>
2021-04-03 17:56:52 +0530

    add checksum chck

--- boot.asm
+++ boot.asm
@@ -5,7 +5,7 @@ section .multiboot               ;according to multiboot spec
         dd 0x1BADB002            ;set magic number for
                                  ;bootloader
         dd 0x0                   ;set flags
-        dd - (0x1BADB002 + 0x0)  ;set checksum
+        dd - (0x1BADB002 + 0x0)  ;set checksum()
 
 section .text
 global start
